95610db053b591cd100f8d876af3fba1fdecd6cd
Ruslan Latypov <rus_91.08@mail.ru>
2018-03-28 20:31:08 +0300

    implemented user's api

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import numeral from 'numeral';
 import './index.css';
+import {getUsers} from './api/usersApi';
 const value = numeral(1000).format('$0,0.00');
 console.log(`Converted numeral value: ${value}`); //eslint-disable-line no-console
+const convertUserToHtml = (user) => `<tr>
+                                       <td><a href="#" data-id=${user.id} class="deleteUser">Delete</a></td>
+                                       <td>${user.id}</td>
+                                       <td>${user.firstName}</td>
+                                       <td>${user.lastName}</td>
+                                       <td>${user.email}</td>
+                                     </tr>`
+getUsers().then(users=>{
+    document.querySelector('#users>tbody').innerHTML = users.map(convertUserToHtml).join('');
+});
